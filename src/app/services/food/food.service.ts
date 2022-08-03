4246416270dcf97c2355833e840609d0e335679c
Harshad <harshad.chavda@techivies.com>
2022-08-03 18:56:35 +0530

    desgin only

--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-
+import { Foods } from 'src/app/shared/models/food';
 @Injectable({
   providedIn: 'root'
 })
@@ -7,18 +7,111 @@ export class FoodService {
 
   constructor() { }
 
-  getAll():string[] {
-    return[
-      '/assets/download(1).jpg',
-      '/assets/download(2).jpg',
-      '/assets/download(3).jpg',
-      '/assets/download(4).jpg',
-      '/assets/download(5).jpg',
-      '/assets/download(6).jpg',
-      '/assets/download(7).jpg',
-      '/assets/download(8).jpg',
-      '/assets/download(9).jpg',
-      '/assets/download.jpg',
+  getAll():Foods[]{
+    return [
+      {
+        id: 1,
+        price: 10,
+        name: 'Pizzza',
+        favorite:false,
+        star: 4.5,
+        origins:['italy'],
+        tags: ['fastfood','pizza','lunch'],
+        imageurl:'/assets/download.jpg'
+      },
+      {
+        id: 2,
+        price: 10,
+        name: 'Pizzza',
+        favorite:false,
+        star: 4.5,
+        origins: ['Italy'] ,
+        tags: ['fastfood','pizza','lunch'],
+        imageurl:'/assets/download(1).jpg'
+
+      },
+      {
+        id: 3,
+        price: 10,
+        name: 'Pizzza',
+        favorite:false,
+        star: 4.5,
+        origins:['italy'],
+        tags: ['fastfood','pizza','lunch'],
+        imageurl:'/assets/download(2).jpg'
+      },{
+        id: 4,
+        price: 10,
+        name: 'Pizzza',
+        favorite:false,
+        star: 4.5,
+        origins:['italy'],
+        tags: ['fastfood','pizza','lunch'],
+        imageurl:'/assets/download(3).jpg'
+
+      },
+      {
+        id: 5,
+        price: 10,
+        name: 'Pizzza',
+        favorite:false,
+        star: 4.5,
+        origins:['italy'],
+        tags: ['fastfood','pizza','lunch'],
+        imageurl:'/assets/download(4).jpg'
+
+      },
+      {
+        id: 6,
+        price: 10,
+        name: 'Pizzza',
+        favorite:false,
+        star: 4.5,
+        origins:['italy'],
+        tags: ['fastfood','pizza','lunch'],
+        imageurl:'/assets/download(5).jpg'
+
+      },
+      {
+        id: 7,
+        price: 10,
+        name: 'Pizzza',
+        favorite:false,
+        star: 4.5,
+        origins:['italy'],
+        tags: ['fastfood','pizza','lunch'],
+        imageurl:'/assets/download(6).jpg'
+      },
+      {
+        id: 8,
+        price: 10,
+        name: 'Pizzza',
+        favorite:false,
+        star: 4.5,
+        origins:['italy'],
+        tags: ['fastfood','pizza','lunch'],
+        imageurl:'/assets/download(7).jpg'
+      },
+      {
+        id: 9,
+        price: 10,
+        name: 'Pizzza',
+        favorite:false,
+        star: 4.5,
+        origins:['italy'],
+        tags: ['fastfood','pizza','lunch'],
+        imageurl:'/assets/download(8).jpg'
+      },
+      {
+        id: 10,
+        price: 10,
+        name: 'Pizzza',
+        favorite:false,
+        star: 4.5,
+        origins:['italy'],
+        tags: ['fastfood','pizza','lunch'],
+        imageurl:'/assets/download(9).jpg'
+      }
     ]
   }
 }
---
+++ src/app/shared/models/food.ts
@@ -0,0 +1,10 @@
+export class Foods {
+    id!:number;
+    price!:number;
+    name!:string;
+    favorite:boolean=false;
+    star:number=0;
+    tags?:string[];
+    imageurl!:string;
+    origins!:string[];
+}
